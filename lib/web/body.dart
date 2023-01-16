5bedaf5bfd3ffdabf9401799f826b79a930666a2
AnaGamplay <anaoliveira360.dev@gmail.com>
2023-01-16 15:54:16 -0400

    provider parte 2

---
+++ lib/web/body.dart
@@ -0,0 +1,15 @@
+import 'package:carros_flutter_web/app_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    AppModel app = Provider.of<AppModel>(context);
+
+    return app.page!;
+  }
+}
