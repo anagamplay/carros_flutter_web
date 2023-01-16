7b59b71a3b1408e80372ab876013bc80b6dc0aff
AnaGamplay <anaoliveira360.dev@gmail.com>
2023-01-16 16:19:26 -0400

    exemplo 1

--- lib/web/menu.dart
+++ lib/web/menu.dart
@@ -14,6 +14,8 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+  String? selected;
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -26,16 +28,23 @@ class _MenuState extends State<Menu> {
   }
 
   _itemMenu(String title, IconData icon, Widget page) {
+
+    bool b = title == selected;
+
     return Material(
-      color: Colors.transparent,
+      color: b ? Theme.of(context).hoverColor : Colors.transparent,
       child: InkWell(
         onTap: () {
           AppModel app = Provider.of<AppModel>(context, listen: false);
           app.setPage(page);
+          
+          setState(() {
+            selected = title;
+          });
         },
         child: ListTile(
           leading: Icon(icon),
-          title: Text(title),
+          title: Text(title, style: TextStyle(fontWeight: b ? FontWeight.bold : FontWeight.normal),),
         ),
       ),
     );
