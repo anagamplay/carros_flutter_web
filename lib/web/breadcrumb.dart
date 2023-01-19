a3e8474314bcec93a93cf41bb2c44a4ba3387dff
AnaGamplay <anaoliveira360.dev@gmail.com>
2023-01-19 11:28:55 -0400

    listview horizontal

--- lib/web/breadcrumb.dart
+++ lib/web/breadcrumb.dart
@@ -1,5 +1,6 @@
 import 'package:carros_flutter_web/app_model.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
 class BreadCrumb extends StatefulWidget {
@@ -12,9 +13,32 @@ class BreadCrumb extends StatefulWidget {
 class _BreadCrumbState extends State<BreadCrumb> {
   @override
   Widget build(BuildContext context) {
-
     AppModel app = Provider.of<AppModel>(context);
 
-    return Text(app.pages.toString());
+    return ListView.builder(
+      itemCount: app.pages.length,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        PageInfo info = app.pages[index];
+
+        return Row(
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints.expand(width: 32),
+              child: Icon(
+                index == 0
+                    ? FontAwesomeIcons.house
+                    : FontAwesomeIcons.chevronRight,
+                size: 20,
+              ),
+            ),
+            Text(
+              info.title,
+              style: TextStyle(fontSize: 17),
+            )
+          ],
+        );
+      },
+    );
   }
 }
